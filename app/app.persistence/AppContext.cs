614b9481640dc07db810c6da2a67adcb42795aac
odemers <odemers@PORTABLE-OLID>
2019-11-26 14:17:18 -0500

    Projet initial.

---
+++ app/app.persistence/AppContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace app.app.persistence
+{
+    public class AppContext : DbContext
+    {
+        // Constructeur
+        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+
+        // Lors de la création d'un modèle, l'ajouter ici en suivant l'exemple ci-dessous :
+        // public DbSet<app.domain.NomDuModèle> nomDuModèle { get; set; }
+        // Le modèle doit absolument hériter de app.persistence.Entity
+    }
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppContext>
+    {
+        public AppContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<AppContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+            return new AppContext(builder.Options);
+        }
+    }
+}
---
+++ app/app.persistence/AppRepository.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app.app.persistence
+{
+    public class AppRepository<T> : IRepository<T> where T : Entity
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public AppRepository(IServiceScopeFactory scopeFactory)
+        {
+            this._scopeFactory = scopeFactory;
+        }
+
+        public async Task Add(T entity)
+        {
+            using (var scope = this._scopeFactory.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppContext>();
+                var entities = db.Set<T>();
+
+                await entities.AddAsync(entity);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T entity)
+        {
+            using (var scope = this._scopeFactory.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppContext>();
+                var entities = db.Set<T>();
+
+                entities.Remove(entity);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IQueryable<T>> GetAll()
+        {
+            using (var scope = this._scopeFactory.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppContext>();
+                var entities = db.Set<T>();
+
+                await db.SaveChangesAsync();
+                return (IQueryable<T>)entities.ToList();
+            }
+        }
+
+        public async Task<T> GetById(int? id)
+        {
+            using (var scope = this._scopeFactory.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppContext>();
+
+                if (id == null || id < 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    var entities = db.Set<T>();
+                    var entity = await entities.FindAsync(id);
+
+                    if (entity == null)
+                    {
+                        return null;
+                    }
+                    return entity;
+                }
+            }
+        }
+
+        public async Task Update(T entity)
+        {
+            using (var scope = this._scopeFactory.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppContext>();
+                var entities = db.Set<T>();
+
+                entities.Update(entity);
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
