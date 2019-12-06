adbac164dc3f978a5c485e6793cec54f8cf882e4
Jean Harghel <harj131@hotmail.com>
2019-12-06 12:48:14 -0500

    .

---
+++ app/app.domain/Models/ListeSignalement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using app.Models;
+
+namespace app.domain.Models
+{
+	public class ListeSignalement : Entity
+	{
+		public string Titre { get; set; }
+		public string Nature { get; set; }
+		public string Commentaire { get; set; }
+		public string Secteur { get; set; }
+		public string Date { get; set; }
+		public string Heure { get; set; }
+	}
+}
+
+
---
+++ app/app.persistence/MockListeSignalementRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using app.persistence;
+using app.domain.Models;
+
+namespace app.persistence
+{
+	public class MockListeSignalementRepository : IRepository<ListeSignalement>
+	{
+		private readonly List<ListeSignalement> _listeSignalement;
+
+		public MockListeSignalementRepository()
+		{
+			_listeSignalement = new List<ListeSignalement>()
+			{
+				new ListeSignalement(){Titre = "Feu", Nature = "soleil", Commentaire = "chaud", Secteur = "Ici", Date = "aujourd'hui", Heure = "maintenant"},
+				new ListeSignalement(){Titre = "blizzard", Nature = "foird", Commentaire = "asd", Secteur = "asd", Date = "asd", Heure = "asd"},
+			};
+
+	}
+
+		public Task Add(ListeSignalement entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task Delete(ListeSignalement entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IQueryable<ListeSignalement>> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ListeSignalement> GetById(int? id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task Update(ListeSignalement entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
