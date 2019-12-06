69b672563f01648349905bc8cf213f954b8b7557
Ezolile <aly.22@hotmail.com>
2019-12-06 10:57:16 -0500

    WIP : Une news contient un titre, un texte, un lien et un état de publication

---
+++ app/app.web/Models/News.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app.web.Models
+{
+    public class News
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Link { get; set; }
+        public string Status { get; set; }
+    }
+}
