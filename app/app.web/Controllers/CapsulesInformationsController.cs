ca625ec5c677a191a92db1ab3bbb232ed48534a3
Jean Harghel <harj131@hotmail.com>
2019-12-10 07:57:20 -0500

    Okay, another step, appContext bug is resolved, now with controller bugs

--- app/app.web/Controllers/CapsulesInformationsController.cs
+++ app/app.web/Controllers/CapsulesInformationsController.cs
@@ -29,19 +29,7 @@ namespace app.web.Controllers
         // GET: CapsulesInformations/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var capsulesInformation = await _context.capsulesInformation
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (capsulesInformation == null)
-            {
-                return NotFound();
-            }
-
-            return View(capsulesInformation);
+            return View();
         }
 
         // GET: CapsulesInformations/Create
@@ -57,29 +45,13 @@ namespace app.web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Titre,Texte,Lien,Status,Id")] CapsulesInformation capsulesInformation)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(capsulesInformation);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(capsulesInformation);
+            return View();
         }
 
         // GET: CapsulesInformations/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var capsulesInformation = await _context.capsulesInformation.FindAsync(id);
-            if (capsulesInformation == null)
-            {
-                return NotFound();
-            }
-            return View(capsulesInformation);
+            return View();
         }
 
         // POST: CapsulesInformations/Edit/5
@@ -89,50 +61,13 @@ namespace app.web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Titre,Texte,Lien,Status,Id")] CapsulesInformation capsulesInformation)
         {
-            if (id != capsulesInformation.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(capsulesInformation);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CapsulesInformationExists(capsulesInformation.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(capsulesInformation);
+            return View();
         }
 
         // GET: CapsulesInformations/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var capsulesInformation = await _context.capsulesInformation
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (capsulesInformation == null)
-            {
-                return NotFound();
-            }
-
-            return View(capsulesInformation);
+            return View();
         }
 
         // POST: CapsulesInformations/Delete/5
@@ -140,15 +75,7 @@ namespace app.web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var capsulesInformation = await _context.capsulesInformation.FindAsync(id);
-            _context.capsulesInformation.Remove(capsulesInformation);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool CapsulesInformationExists(int id)
-        {
-            return _context.capsulesInformation.Any(e => e.Id == id);
+            return View();
         }
     }
 }
