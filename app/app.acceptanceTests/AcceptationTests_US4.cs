c3bd391ed4767bd2219f654bd4bde0d91723997f
Jean Harghel <harj131@hotmail.com>
2019-12-11 07:54:20 -0500

    added all tests

---
+++ app/app.acceptanceTests/AcceptationTests_US4.cs
@@ -0,0 +1,56 @@
+﻿using app.persistence;
+using app.web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace app.unitTests
+{
+    [Story(
+            Title = "Je veux pouvoir voir les infos d'une capsule",
+            AsA = "User général",
+            IWant = "Je veux les infos d'une capsule,",
+            SoThat = "Afin de pouvoir prendre connaissance d'une capsule")]
+
+    public class NewsTestsAcceptation
+    {
+        public domain.Models.CapsulesInformation capsule = new domain.Models.CapsulesInformation();
+        public MockCapsulesInformationRepository mockRepo;
+        public CapsulesInformationsController controller;
+        public IActionResult result;
+
+        [Fact]
+        public void visualiser_les_infos_d_une_news()
+        {
+            this.Given(x => une_capsule())
+                .When(x => l_user_demande_de_voir_la_liste_des_capsules())
+                .Then(x => les_infos_d_une_capsule())
+                .BDDfy();
+        }
+
+        private void une_capsule()
+        {
+            capsule.Titre = "Folie contagieuse";
+            capsule.Texte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquet justo vitae dictum vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Cras condimentum justo eu dolor iaculis pretium.Ut magna diam, gravida sit amet posuere id, semper nec nisi.Suspendisse vitae leo interdum, faucibus leo ac, sagittis nisl.Aenean aliquam ligula erat, et eleifend justo tempus in. Curabitur condimentum rutrum efficitur. Nulla ante enim, aliquam eget posuere eget, fringilla at est. Pellentesque interdum, arcu a lobortis vestibulum, dui est fermentum est, vel fermentum turpis neque eget tortor.Vestibulum blandit eros turpis, non consequat ligula vehicula in. Nullam porttitor metus vel iaculis eleifend. Quisque vel odio id enim tempus varius ut non tellus. Nulla eget maximus sem. Sed aliquam velit quis libero pharetra auctor.Morbi condimentum, lacus nec lobortis faucibus, orci lectus bibendum massa, et pellentesque nisi risus id magna.Morbi at ante faucibus, ullamcorper metus id, volutpat ligula.Vivamus efficitur at neque id varius. Aenean et justo sollicitudin, mattis eros ut, luctus velit.Aliquam erat volutpat.Ut faucibus, velit venenatis pellentesque ornare, diam libero egestas arcu, ac vehicula ex magna sit amet dolor. Cras non aliquam dui. Cras interdum sodales venenatis. Praesent eget porttitor.";
+            capsule.Lien = "https://www.youtube.com/watch?v=RJIDLGuqfmc";
+            capsule.Status = "published";
+        }
+
+        private async Task l_user_demande_de_voir_la_liste_des_capsules()
+        {
+            // Arrange
+            mockRepo = new MockCapsulesInformationRepository();
+            controller = new CapsulesInformationsController(mockRepo);
+
+            // Act
+            result = await controller.Index();
+        }
+
+        private async void les_infos_d_une_capsule()
+        {
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ app/app.interfaceTests/InterfaceTests_US-4.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace app.interfaceTests
+{
+    public class CapsulesInterfaceTests
+    {
+        private IWebDriver driver;
+
+        //private const string FIREFOX_EXE_PATH = "C:\Program Files\Mozilla Firefox\firefox.exe";
+        private const string APP_CAPSULES_URL = "https://localhost:44341/Home/Capsules";
+        private const string ADD_CAPSULES_URL = "https://localhost:44341/Home/Capsles/Create";
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver("./");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Check_AddCapsules_ShouldRedirectToChangeAddCapsulesPage()
+        {
+            driver.Navigate().GoToUrl(APP_CAPSULES_URL);
+
+            //Click Add capsules
+            var adddCapsulesLink = driver.FindElement(By.PartialLinkText("Créer"));
+            adddCapsulesLink.Click();
+
+            Assert.AreEqual(ADD_CAPSULES_URL, adddCapsulesLink);
+            driver.Quit();
+        }
+
+        [Test]
+        public void Check_DeleteCapsules_ShouldRedirectToChangeDeleteCapsulesPage()
+        {
+            driver.Navigate().GoToUrl(APP_CAPSULES_URL);
+
+            //Click Add capsules
+            var deleteCapsulesLink = driver.FindElement(By.PartialLinkText("Delete"));
+            deleteCapsulesLink.Click();
+
+            Assert.AreEqual(ADD_CAPSULES_URL, deleteCapsulesLink);
+            driver.Quit();
+        }
+
+        [Test]
+        public void Check_EditCapsules_ShouldRedirectToChangeEditCapsulesPage()
+        {
+            driver.Navigate().GoToUrl(APP_CAPSULES_URL);
+
+            //Click Add capsules
+            var EditCapsulesLink = driver.FindElement(By.PartialLinkText("Edit"));
+            EditCapsulesLink.Click();
+
+            Assert.AreEqual(ADD_CAPSULES_URL, EditCapsulesLink);
+            driver.Quit();
+        }
+    }
+}
