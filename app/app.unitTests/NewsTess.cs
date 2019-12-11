1408b49f47ba4b8446bca7769b669a37c62aa294
Alyssa <32277824+Ezolile@users.noreply.github.com>
2019-12-11 07:15:38 -0500

    WIP : Add unit tests

---
+++ app/app.unitTests/NewsTess.cs
@@ -0,0 +1,60 @@
+﻿using app.persistence;
+using app.web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace app.unitTests
+{
+    class NewsTess
+    {
+        [Fact]
+        public async Task Test_News_Index_Returns_A_ViewResult()
+        {
+            // Arrange
+            var mockRepo = new MockNewsRepository();
+            var controller = new NewsController(mockRepo);
+
+            // Act
+            var result = await controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public async Task Test_News_Index_Model_Is_An_Enumerable_Model_of_News()
+        {
+            // Arrange
+            var mockRepo = new MockNewsRepository();
+            var controller = new NewsController(mockRepo);
+
+            //Act
+            var result = await controller.Index() as ViewResult;
+            var model = result.ViewData.Model;
+
+            //Assert
+            Assert.IsAssignableFrom<IEnumerable<domain.News>>(model);
+        }
+
+        [Fact]
+        public async Task Test_Index_Model_Contains_News()
+        {
+            // Arrange
+            var mockRepo = new MockNewsRepository();
+            var controller = new NewsController(mockRepo);
+
+            //Act
+            var result = await controller.Index() as ViewResult;
+            var model = result.ViewData.Model;
+            var list = model as IEnumerable<domain.News>;
+
+            //Assert
+            Assert.Equal(3, list.Count());
+        }
+    }
+}
